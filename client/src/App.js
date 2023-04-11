dde652c41fcf5a76dc32bb8b1840e2d37a2e6000
Vishwas.V.Bharadwaj <Vishwasvb1997@gmail.com>
2023-04-11 17:10:56 +0530

    signIn and signUp screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Switch, Route } from "react-router-dom"; 
+
 import Landing from './components/Landing/Landing';
+import SignIn from "./components/SignIn/SignIn";
+import Singup from "./components/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Landing/>
-    </div>
+    // <SignIn />
+    <Singup />
+    // <Switch>
+    //   <Route path="/" component={Landing} />
+    //   <Route path="/signIn" component={SignIn} />
+    // </Switch>
   );
 }
 
