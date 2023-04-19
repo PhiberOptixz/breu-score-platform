6d63d7dc221bcfe42bc6c574b54f8a5679aeec9b
JeevanAnand <jeev46@gmail.com>
2023-04-19 17:08:47 +0530

    Reliability barebone

---
+++ Breu_web_code/client/src/components/Reliability/Reliability.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Header from "../../common/header";
+
+
+
+
+const Reliability = () => {
+
+  return (
+    <>
+      <Header name="Reliability"/>
+      <Grid container className="believabilityContainer">
+
+      </Grid>
+    </>
+  );
+};
+export default Reliability;
